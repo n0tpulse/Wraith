1a5fb51e85149ae8a443b66bf6db16fc980059e2
Pulse <83741263+n0tpulse@users.noreply.github.com>
2023-01-07 17:59:38 -0500

    update

---
+++ connect.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');;
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('connect')
+        .setDescription('Provides a connect code to the server'),
+    async execute(interaction) {
+        //Wwrite a button that says "Join Here" and when clicked, it sends the user to the connect codes location
+        const row = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setLabel('Join Here!')
+                .setStyle(ButtonStyle.Link)
+                .setURL('Add your Server CFX.re/join Link Here!')
+        );
+
+    await interaction.reply({ content: 'Join The server by clicking below!', components: [row] });
+}
+};
---
+++ count.js
@@ -0,0 +1,22 @@
+//write a command to grab the number of members in a fivem server from the API here https://servers-frontend.fivem.net/api/servers/single/lbjmv7 and display it in an embed using undici
+const { SlashCommandBuilder, PermissionFlagsBits, ChatInputCommandInteraction, EmbedBuilder } = require('discord.js');
+const { request } = require('undici');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('count')
+        .setDescription('Get the number of players on the server.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+            .setTitle("Server Player Count")   
+            .setColor("Blue")
+            .setTimestamp()
+            .setFooter({ text: "Requested by " + interaction.user.tag })
+            const server = await request('http://serverip:port/dynamic.json')
+            const players = await server.body.json()
+            embed.setDescription(`There are currently ${players.clients}/${players.sv_maxclients} players on the server.`);
+            interaction.reply({ embeds: [embed] });
+    },
+
+
+};
